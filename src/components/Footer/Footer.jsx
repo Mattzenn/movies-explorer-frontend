97cd54fd8057e89455824ed3553277d313b1baed
Matvey Zenin <79812383+Mattzenn@users.noreply.github.com>
2022-05-18 17:17:03 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
             <div className="footer__container">
                 <span className="footer__date">&copy; {new Date().getFullYear()}</span>
                 <div className="footer__items">
-                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="">Яндекс.Практикум</a></div>
-                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="">Github</a></div>
-                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="">Facebook</a></div>
+                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="https://practicum.yandex.ru/">Яндекс.Практикум</a></div>
+                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="https://github.com/Mattzenn">Github</a></div>
+                    <div className="footer__item"><a target="_blank" rel="noreferrer" className="footer__link" href="https://t.me/matvey_zenin">Telegram</a></div>
                 </div>
             </div>
         </section>
